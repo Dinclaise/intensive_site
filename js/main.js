f2fdd5f3ae2ec31ba4398e87c360963a7ba400d7
Anton <aanohin98@gmail.com>
2019-02-08 15:38:38 +0300

    0.7.0
    1) Added styles for counter-minus.
    2) Added scripts for quantity and cart

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
-/* JS code */
+$(document).ready(function(){
+
+	var quantity = 1;
+
+
+
+	$('#minus').click(function(){
+
+		if ( quantity > 0) {
+
+			quantity--;
+		}
+
+		$('#quantityCell').text(quantity);
+
+	});
+
+
+
+	$('#plus').click(function(){
+
+		quantity++;
+
+		$('#quantityCell').text(quantity);
+	});
+
+
+		var cartItems = 0;
+		var cartTotalPrice = 0;
+
+		$('#items-count').text(cartItems);
+
+		$('#price-total').text(cartTotalPrice);
+
+		$('#addToCart').click(function(){
+			console.log('Click Add to cart!!!');
+		})
+
+
+
+});
