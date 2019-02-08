3458fe25e35543bef362454be96299b28b0fbe79
Anton <aanohin98@gmail.com>
2019-02-08 16:22:43 +0300

    0.7.5
    Finished script ( can add style)

--- js/main.js
+++ js/main.js
@@ -27,15 +27,56 @@ $(document).ready(function(){
 
 		var cartItems = 0;
 		var cartTotalPrice = 0;
+		var priceOneItem = $('#price').attr('data-price'); // 500
 
-		$('#items-count').text(cartItems);
+		$('#items-count').text(cartItems); // 0
 
-		$('#price-total').text(cartTotalPrice);
+		$('#price-total').text(cartTotalPrice); // 0
 
 		$('#addToCart').click(function(){
-			console.log('Click Add to cart!!!');
-		})
+
+			cartItems = cartItems + quantity; // 5
+
+			cartTotalPrice = cartItems * priceOneItem + '$'; // 5 * 500 = 2500
 
 
+			$('#items-count').text(cartItems);
+
+			$('#price-total').text(cartTotalPrice);
+
+
+			quantity = 1;
+
+			$('#quantityCell').text(quantity);
+
+
+			if( cartItems > 0) {
+
+				$('#cart-empty').hide();
+
+				$('#cart-with-items').show();
+
+				$('#clear-cart').show();
+			}
+
+		});
+
+
+		$('#clear-cart').click(function(){
+
+			cartItems = 0;
+			cartTotalPrice = 0;
+
+
+			$('#items-count').text(cartItems);
+
+			$('#price-total').text(cartTotalPrice);
+
+
+			$('#cart-with-items').hide();
+			$('#cart-empty').show();
+			$('#clear-cart').hide();
+
+		});
 
 });
